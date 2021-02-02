7798e4b0a448144ea748e18b527af0f531712743
Will Browning <will@willbrowning.me>
2021-02-02 14:47:12 +0000

    Fixed some CLS issues

--- source/_shared/header.blade.php
+++ source/_shared/header.blade.php
@@ -1,6 +1,6 @@
 <header class="flex items-center gradient lg:h-20 py-4">
     <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
-        <div class="flex items-center">
+        <div class="flex items-center h-8 w-48">
             <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
                 <img class="h-8 mr-3" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />
             </a>
