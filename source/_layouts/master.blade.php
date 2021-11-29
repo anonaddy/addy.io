bfb612c4b3139ef03a0c2a4cd2a34f14f6820671
Will Browning <will@willbrowning.me>
2021-11-29 22:08:07 +0000

    Removed BF banner

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -34,20 +34,6 @@
     </head>
 
     <body class="{{ $page->isActive('/') ? 'gradient text-white' : 'flex flex-col justify-between min-h-screen bg-white text-grey-800' }} leading-normal font-sans">
-        <div class="relative bg-pink-600">
-            <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-                <div class="pr-16 sm:text-center sm:px-16">
-                <p class="font-medium text-white mb-0">
-                    <span class="inline">
-                        Black Friday Offer (<b>Ends Today</b>)! Get 50% off your first year with code
-                    </span>
-                    <span class="block ml-1 sm:inline-block text-white font-bold tracking-wide">
-                        BLACKFRIDAY21
-                    </span>
-                </p>
-                </div>
-            </div>
-        </div>
         @include('_shared.header')
 
         @include('_nav.menu-mobile')
