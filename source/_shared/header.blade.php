91d75b53db29a68f4cd4a13c3a396ae3336bdeca
Will Browning <will@willbrowning.me>
2023-08-22 16:33:18 +0100

    Fixed mobile menu logo link

--- source/_shared/header.blade.php
+++ source/_shared/header.blade.php
@@ -28,7 +28,7 @@
         <div class="fixed inset-0 z-10"></div>
         <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-grey-900/10">
             <div class="flex items-center gap-x-6">
-                <a href="#" class="-m-1.5 p-1.5">
+                <a href="/" class="-m-1.5 p-1.5">
                     <span class="sr-only">{{ $page->siteName }}</span>
                     <img class="h-8 w-auto" src="/icon.svg" alt="{{ $page->siteName }} icon">
                 </a>
