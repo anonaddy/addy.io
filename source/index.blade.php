19012ac8076dc8ee7e7717d86d7ee8ddf06ebe79
Will Browning <will@willbrowning.me>
2024-08-15 09:05:13 +0100

    Fixed typo

--- source/index.blade.php
+++ source/index.blade.php
@@ -379,7 +379,7 @@ width: full
                                     <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
                                 </svg>
                                 <span>
-                                    <b>5</b> Additional Username
+                                    <b>5</b> Additional Usernames
                                 </span>
                             </li>
                             <li class="flex gap-x-3">
