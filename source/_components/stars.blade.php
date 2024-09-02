0143b7bb4a825e25b332157a17262685df481567
Will Browning <will@willbrowning.me>
2024-09-02 15:06:28 +0100

    Added Safari extension link and reviews section

---
+++ source/_components/stars.blade.php
@@ -0,0 +1,17 @@
+<div class="text-grey-600 text-nowrap">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 text-yellow-300 inline-block">
+        <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 text-yellow-300 inline-block">
+        <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 text-yellow-300 inline-block">
+        <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 text-yellow-300 inline-block">
+        <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 text-yellow-300 inline-block">
+        <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
+    </svg>
+</div>
--- source/index.blade.php
+++ source/index.blade.php
@@ -136,7 +136,7 @@ width: full
                         <h3 class="text-3xl text-grey-800 font-bold leading-none mb-3">Browser Extension</h3>
                         <p class="text-grey-600 mb-4">Generate new aliases in a couple of clicks straight from your
                             browser using the <a href="https://github.com/anonaddy/browser-extension" class="text-indigo-700 font-bold" target="_blank" rel="nofollow noopener noreferrer">open-source</a> browser extension.</p>
-                        <p class="text-grey-600 b-4">The extension is available for download on <a href="https://addons.mozilla.org/en-GB/firefox/addon/addy_io/" class="text-indigo-700 font-bold" target="_blank" rel="nofollow noopener noreferrer">Firefox</a>, <a href="https://chrome.google.com/webstore/detail/addyio-anonymous-email-fo/iadbdpnoknmbdeolbapdackdcogdmjpe" class="text-indigo-700 font-bold" target="_blank" rel="nofollow noopener noreferrer">Chrome</a> and <a href="https://microsoftedge.microsoft.com/addons/detail/addyio-anonymous-email/ohjlgpcfncgkijjfmabldlgnccmgcehl" class="text-indigo-700 font-bold" target="_blank" rel="nofollow noopener noreferrer">Edge</a>!</p>
+                        <p class="text-grey-600 b-4">The extension is available for download on <a href="https://addons.mozilla.org/en-GB/firefox/addon/addy_io/" class="text-indigo-700 font-bold" target="_blank" rel="nofollow noopener noreferrer">Firefox</a>, <a href="https://chrome.google.com/webstore/detail/addyio-anonymous-email-fo/iadbdpnoknmbdeolbapdackdcogdmjpe" class="text-indigo-700 font-bold" target="_blank" rel="nofollow noopener noreferrer">Chrome</a>, <a href="https://microsoftedge.microsoft.com/addons/detail/addyio-anonymous-email/ohjlgpcfncgkijjfmabldlgnccmgcehl" class="text-indigo-700 font-bold" target="_blank" rel="nofollow noopener noreferrer">Edge</a> and <a href="https://apps.apple.com/app/addy-io-extension/id6670220050" class="text-indigo-700 font-bold" target="_blank" rel="nofollow noopener noreferrer">Safari</a>!</p>
                         <p class="text-grey-600 mb-4">Also available on other chromium based browsers such as Brave and
                             Vivaldi.</p>
                     </div>
@@ -253,8 +253,7 @@ width: full
                         <h3 class="text-3xl text-grey-800 font-bold leading-none mb-3">API Access</h3>
                         <p class="text-grey-600 mb-4">Manage your aliases, recipients, domains and additional usernames
                             using the addy.io API.</p>
-                        <p class="text-grey-600 mb-4">In order to use the API you first need to generate an API access
-                            token in your account settings.</p>
+                        <p class="text-grey-600 mb-4">In order to use the API you first need to generate an API Key in your account settings.</p>
                         <p class="text-grey-600 mb-4"><a href="{{ $page->appUrl }}/docs/" class="text-indigo-700 font-bold">View API documentation</a></p>
                     </div>
                 </div>
@@ -264,6 +263,128 @@ width: full
             </div>
 
     </div>
+</section>
+
+<section class="relative isolate bg-white pb-32 pt-24 sm:pt-32">
+  <div class="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl" aria-hidden="true">
+    <div class="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-gradient-to-tr from-[#3AE7E1] to-[#4055A8]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+  </div>
+  <div class="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end" aria-hidden="true">
+    <div class="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-[#3AE7E1] to-[#4055A8] xl:ml-0 xl:mr-[calc(50%-12rem)]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+  </div>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-xl text-center">
+      <h2 class="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Don't just take our word for it!</h2>
+      <p class="mt-2 text-3xl font-bold tracking-tight text-grey-900 sm:text-4xl">Join thousands of happy users</p>
+    </div>
+    <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-grey-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
+      <figure class="rounded-2xl bg-white shadow-lg ring-1 ring-grey-900/5 sm:col-span-2 xl:col-start-2 xl:row-end-1">
+        <div class="p-6 text-lg leading-7 tracking-tight text-grey-900 sm:p-12 sm:text-xl sm:leading-8">
+            <p>“I've been using Addy.io for a few years now and it's been absolutely brilliant. Well worth the small amount of money for additional premium features which it seems are being increased and improved on a regular basis. This extension just makes the service even better. Great work!”</p>
+        </div>
+        <figcaption class="flex flex-wrap items-center gap-x-4 gap-y-4 border-t border-grey-900/10 px-6 py-4 sm:flex-nowrap">
+          <img class="h-10 w-10 flex-none rounded-full bg-grey-50" src="/assets/img/reviews/grant.png" alt="">
+          <div class="flex-auto">
+            <div class="font-semibold">Grant Marshall</div>
+            @include('_components.stars')
+          </div>
+        </figcaption>
+      </figure>
+      <div class="space-y-8 xl:contents xl:space-y-0">
+        <div class="space-y-8 xl:row-span-2">
+          <figure class="rounded-2xl bg-white p-6 shadow-lg ring-1 ring-grey-900/5">
+            <div class="text-grey-900">
+                <p>“Great service. Efficient, customer services fast and easy to use. Kudos.”</p>
+            </div>
+            <figcaption class="mt-6 flex items-center gap-x-4">
+            <img class="h-10 w-10 rounded-full bg-grey-50" src="/assets/img/reviews/lorenzo.png" alt="">
+              <div>
+                <div class="font-semibold">Lorenzo</div>
+                @include('_components.stars')
+              </div>
+            </figcaption>
+          </figure>
+
+          <figure class="rounded-2xl bg-white p-6 shadow-lg ring-1 ring-grey-900/5">
+            <div class="text-grey-900">
+              <p>“Great for protecting your real email address. Browser extension works perfectly.”</p>
+            </div>
+            <figcaption class="mt-6 flex items-center gap-x-4">
+              <img class="h-10 w-10 rounded-full bg-grey-50" src="/assets/img/reviews/kieran.jpg" alt="">
+              <div>
+                <div class="font-semibold">Kieran Colfer</div>
+                @include('_components.stars')
+              </div>
+            </figcaption>
+          </figure>
+          <!-- More testimonials... -->
+        </div>
+        <div class="space-y-8 xl:row-start-1">
+          <figure class="rounded-2xl bg-white p-6 shadow-lg ring-1 ring-grey-900/5">
+            <div class="text-grey-900">
+              <p>“This is a must have tool for all privacy conscious people on the web.”</p>
+            </div>
+            <figcaption class="mt-6 flex items-center gap-x-4">
+              <img class="h-10 w-10 rounded-full bg-grey-50" src="/assets/img/reviews/jurgen.webp" alt="">
+              <div>
+                <div class="font-semibold">Jürgen V.</div>
+                @include('_components.stars')
+              </div>
+            </figcaption>
+          </figure>
+
+          <!-- More testimonials... -->
+        </div>
+      </div>
+      <div class="space-y-8 xl:contents xl:space-y-0">
+        <div class="space-y-8 xl:row-start-1">
+          <figure class="rounded-2xl bg-white p-6 shadow-lg ring-1 ring-grey-900/5">
+            <div class="text-grey-900">
+              <p>“Smooth and easy in use. No missing emails. I became a happy paying customer.”</p>
+            </div>
+            <figcaption class="mt-6 flex items-center gap-x-4">
+              <img class="h-10 w-10 rounded-full bg-grey-50" src="/assets/img/reviews/dan.png" alt="">
+              <div>
+                <div class="font-semibold">Dan</div>
+                @include('_components.stars')
+              </div>
+            </figcaption>
+          </figure>
+
+          <!-- More testimonials... -->
+        </div>
+        <div class="space-y-8 xl:row-span-2">
+          <figure class="rounded-2xl bg-white p-6 shadow-lg ring-1 ring-grey-900/5">
+            <div class="text-grey-900">
+              <p>“Powerful, and has a gorgeous GUI.”</p>
+            </div>
+            <figcaption class="mt-6 flex items-center gap-x-4">
+              <img class="h-10 w-10 rounded-full bg-grey-50" src="/assets/img/reviews/roke.png" alt="">
+              <div>
+                <div class="font-semibold">`{first: "Roke"}`{.JSONC}</div>
+                @include('_components.stars')
+              </div>
+            </figcaption>
+          </figure>
+
+          <figure class="rounded-2xl bg-white p-6 shadow-lg ring-1 ring-grey-900/5">
+            <div class="text-grey-900">
+              <p>“I love this service. Solved my whole spam problem. The developer seems dedicated and passionate about security and privacy.”</p>
+            </div>
+            <figcaption class="mt-6 flex items-center gap-x-4">
+            <img class="h-10 w-10 rounded-full bg-grey-50" src="/assets/img/reviews/R.png" alt="">
+              <div>
+                <div class="font-semibold">Richard</div>
+                @include('_components.stars')
+              </div>
+            </figcaption>
+          </figure>
+
+          <!-- More testimonials... -->
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 
 <section id="pricing" class="bg-white py-24 sm:py-32">
