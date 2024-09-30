3cf9a6355f51a559bb319f479d82406e5cfd3a2c
Will Browning <will@willbrowning.me>
2024-09-30 21:42:11 +0100

    Updated js

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -15,4 +15,11 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 if (params.ref === 'appstore') {
   document.getElementById('pricing-nav-link').remove()
   document.getElementById('pricing-menu-item').remove()
+
+  document.getElementById('help-nav-link').href = '/help/?ref=appstore'
+  document.getElementById('help-menu-item').href = '/help/?ref=appstore'
+  document.getElementById('faq-nav-link').href = '/faq/?ref=appstore'
+  document.getElementById('faq-menu-item').href = '/faq/?ref=appstore'
+  document.getElementById('blog-nav-link').href = '/blog/?ref=appstore'
+  document.getElementById('blog-menu-item').href = '/blog/?ref=appstore'
 }
